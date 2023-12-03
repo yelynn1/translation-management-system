78b24d52260a7bae5727a9434003b4c9e788f526
Ye Lynn Khant <koyei35@gmail.com>
2023-12-03 16:33:37 +0800

    Init frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use('/', routes);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "antd";
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Main App</h1>
+      <Button type="primary">Button</Button>
+    </div>
+  );
+};
+
+export default App;
